5fdaafd07cbb0ce92ad84f58a6c34724d4e92c65
Kughan <kughanravindran@gmail.com>
2018-07-16 11:09:25 +0800

    inf2bcw1

---
+++ src/imult/StudentCode.java
@@ -0,0 +1,32 @@
+package imult;
+/*
+ * Class StudentCode: class for students to implement
+ * the specific methods required by the assignment:
+ * add()
+ * sub()
+ * koMul()
+ * koMulOpt()
+ * (See coursework handout for full method documentation)
+ */
+
+public class StudentCode {
+  public static BigInt add(BigInt A, BigInt B) {
+    // student needs to implement
+  }
+
+  public static BigInt sub(BigInt A, BigInt B) {
+    // student needs to implement
+  }
+
+  public static BigInt koMul(BigInt A, BigInt B) {
+    // student needs to implement
+  }
+
+  public static BigInt koMulOpt(BigInt A, BigInt B) {
+    // student needs to implement
+  }
+
+  public static void main(String argv[]) throws java.io.FileNotFoundException {
+
+  }
+} //end StudentCode class
